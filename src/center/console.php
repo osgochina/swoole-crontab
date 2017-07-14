a9cbdd1f63f1f15c677e91f4e34cf71c7636ace9
ClownFish <osgochina@gmail.com>
2017-07-14 10:15:29 +0800

    清除日志

--- src/center/console.php
+++ src/center/console.php
@@ -6,6 +6,14 @@
  * Time: 下午2:18
  */
 require_once __DIR__ . '/_init.php';
-$client = new Lib\Client(CENTER_HOST,CENTRE_PORT);
-$ret = $client->call("Termlog::cleanLogs")->getResult(30);
+function cleanLogs()
+{
+    $datetime = date("Y-m-d", strtotime(date("Y-m-d") . "-1 month"));
+    $db = table("term_logs");
+    if (!$db->dels(["where" => ["createtime<'" . $datetime . "'"]])) {
+        return Lib\Util::errCodeMsg(1, "删除失败");
+    }
+    return Lib\Util::errCodeMsg(0, "删除成功");
+}
+$ret = cleanLogs();
 var_dump($ret);
